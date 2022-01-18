a264bab0c395e36850ac0ef7ade75fd40faf8120
brunoro7 <bruno.ro7c@gmail.com>
2022-01-18 19:46:25 -0300

    entregando requisito 1.

--- src/average.js
+++ src/average.js
@@ -12,8 +12,16 @@
     - average([1, '2']) // Retorno: undefined;
 */
 
-const average = () => {
-  
+const average = (array) => {
+  if (array.length <= 0) return undefined;
+  let somaArrayIndex = 0;
+  for (let i = 0; i < array.length; i += 1) {
+    if (typeof array[i] !== 'number') {
+      return undefined;
+      }
+    somaArrayIndex += array[i];
+  }
+  let media = somaArrayIndex / array.length;
+  return Math.round(media);
 };
-
 module.exports = average;
