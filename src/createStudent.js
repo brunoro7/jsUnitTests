a5f7790ec0e15a36c1142dc9fea73f042d811ddf
brunoro7 <bruno.ro7c@gmail.com>
2022-01-19 04:02:08 -0300

    entregando requisito 5.

--- src/createStudent.js
+++ src/createStudent.js
@@ -15,6 +15,14 @@
     estudante.feedback() // Retorna: 'Eita pessoa boa!'
 */
 
-const createStudent = () => {};
+const createStudent = (nome) => {
+  let student = {
+    name: '', 
+    feedback: '',
+  };
+  student.name = nome;
+  student.feedback = () => 'Eita pessoa boa!';
 
+  return student;
+};
 module.exports = createStudent;
